a734f71d14b69e4ed488420e02acdfff90215e93
igor <igor.homichenko123@gmail.com>
2023-10-01 23:13:37 +0300

    start

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <main>
+            <h1>Home</h1>
+            <p>This is the home page.</p>
+            <p>
+                <a href="/about">About</a>
+            </p>
+            <p>
+                <a href="/contact">Contact</a>
+            </p>
+        </main>
+    );
+}
